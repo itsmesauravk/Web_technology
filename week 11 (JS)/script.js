3893470ca6591bba06086b9f26645635f42e706e
itsmesauravk <np03cs4s230139@heraldcollege.edu.np>
2023-05-09 23:51:29 +0545

    nothing

---
+++ week 11 (JS)/script.js
@@ -0,0 +1,25 @@
+// function sum(a,b) {
+//     let sum = a+b;
+//     console.log("The sum is :"+ sum)
+// }
+// sum(3,5)
+
+
+// function sum() {
+//     let user = prompt("enter the ending num:");
+//     for (let i=1; i<=user; i++) {
+//         if (i%2 == 0){
+//             console.log(i)
+//     }
+// }
+// }
+// sum()
+
+const car = {
+    color : "black",
+    brand : "BM Dablu",
+    price : "1 crore",
+}
+console.log(car.color)
+
+ 
